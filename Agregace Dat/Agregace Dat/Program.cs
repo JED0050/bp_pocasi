8a465853a5cd001d38c42e9a445c232af4065cbc
JED0050 <honzonarr@seznam.cz>
2020-05-28 17:42:47 +0200

    XML yr.no Praha
    Přidání třídy reprezentující předpověď.
    Přidání rozhraní pro načítání dat z různých zdrojů.
    Přidání třídy pro načítání předpovědí z norské meteorologické stanice (yr.no, XML).

--- Agregace Dat/Agregace Dat/Program.cs
+++ Agregace Dat/Agregace Dat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AgregaceDatLib;
 
 namespace Agregace_Dat
 {
@@ -6,7 +7,23 @@ namespace Agregace_Dat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            XMLDataLoader loader = new XMLDataLoader("http://www.yr.no/place/Czech_Republic/Prague/Prague/forecast.xml");
+
+            //string xmlFile = loader.GetXML();
+            //Console.WriteLine(xmlFile);
+
+            Console.WriteLine("Teplota v Praze v tuto chvíli");
+            Console.WriteLine("Teplota: " + loader.GetForecastByTimeInPrague(DateTime.Now).Temperature);
+
+
+            Console.WriteLine("Teplota v Praze za 2 dny v 7:15");
+            DateTime tommorow = DateTime.Now.AddDays(2);
+            TimeSpan hours = new TimeSpan(7, 15, 0);
+            tommorow = tommorow.Date + hours;
+            Console.WriteLine("Teplota: " + loader.GetForecastByTimeInPrague(tommorow).Temperature);
+
+
         }
     }
 }
---
+++ Agregace Dat/AgregaceDatLib/AgregaceDatLib.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace AgregaceDatLib
+{
+
+    //XML
+    //Yr.no - https://www.yr.no/place/Norway/Vestfold_og_Telemark/Midt-Telemark/Gvarv/forecast.xml
+    //https://www.yr.no/place/Czech_Republic/Olomouc/Olomouc/forecast.xml
+    //http://www.yr.no/place/Czech_Republic/Prague/Prague/forecast.xml
+
+    public interface DataLoader
+    {
+
+        Forecast GetForecastByTimeInPrague(DateTime t);
+
+    }
+
+    public class XMLDataLoader : DataLoader
+    {
+        public string UrlAdress { get; set; }
+
+        public XMLDataLoader(string uA)
+        {
+            UrlAdress = uA;
+        }
+
+
+
+        public string GetXML()
+        {
+            string text = "";
+
+            using (var client = new WebClient())
+            {
+                text = client.DownloadString(UrlAdress);
+            }
+
+            return text;
+        }
+
+        public Forecast GetForecastByTimeInPrague(DateTime t)
+        {
+            Forecast f = new Forecast();
+
+            TextReader tr = new StringReader(GetXML());
+
+
+            XDocument xmlDoc = XDocument.Load(tr);
+
+
+            foreach (var weatherData in xmlDoc.Descendants("weatherdata"))
+            {
+                int counter = 0;
+
+                foreach (var location in xmlDoc.Descendants("location"))
+                {
+                    if(counter == 0)
+                    {
+                        f.Country = location.Element("country").Value;
+                        f.City = location.Element("name").Value;
+
+                        counter++;
+                    }else
+                    {
+
+                        f.Latitude = location.Attribute("latitude").Value;
+                        f.Longitude = location.Attribute("longitude").Value;
+                    }
+                }
+
+                counter = 0;
+
+                foreach (var forecast in xmlDoc.Descendants("forecast"))
+                {
+                    foreach (var tabular in xmlDoc.Descendants("tabular"))
+                    {
+                        foreach (var time in xmlDoc.Descendants("time"))
+                        {
+                            if(counter == 0)    //nastavení první hodnoty pokud není žádná jiná známá
+                            {
+                                counter++;
+
+                                f.Temperature = Double.Parse(time.Element("temperature").Attribute("value").Value.Replace('.', ','));
+                                f.Precipitation = Double.Parse(time.Element("precipitation").Attribute("value").Value.Replace('.', ','));
+
+                                continue;
+                            }
+
+                            DateTime from = DateTime.Parse(time.Attribute("from").Value);
+                            DateTime to = DateTime.Parse(time.Attribute("to").Value);
+
+                            if(t >= from && t <= to)
+                            {
+                                f.Temperature = Double.Parse(time.Element("temperature").Attribute("value").Value.Replace('.', ','));
+                                f.Precipitation = Double.Parse(time.Element("precipitation").Attribute("value").Value.Replace('.', ','));
+
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            return f;
+        }
+
+        
+
+    }
+
+    public class Forecast
+    {
+
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Latitude { get; set; }        //zeměpisná šířka
+        public string Longitude { get; set; }       //zeměpisná délka
+        public DateTime Time { get; set; }
+        public double Temperature { get; set; }     //teplota celsius
+        public double Precipitation { get; set; }   //srážky = slabá (0,1 – 2,5), mírná	(2,6 – 8), silná (8 – 40), velmi silná (> 40)
+
+    }
+}
