094f0507f3efe7092040a719eb4c30225c17f7c4
JED0050 <honzonarr@seznam.cz>
2020-05-28 15:35:44 +0200

    Vytvoření projektu agregace dat
    Vytvoření konzolové aplikace pro testování knihovny tříd, která se bude starat o agregaci dat z různých zdrojů ve formátu XML, JSON a Bitmap.

---
+++ Agregace Dat/Agregace Dat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Agregace_Dat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
