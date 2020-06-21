9ce731dabdf670349a9b322866ba9ca55430950b
JED0050 <honzonarr@seznam.cz>
2020-06-21 15:15:29 +0200

    Vytvoření webové služby
    Přidání nového C# ASP.NET projektu. Jedná se o MVC Webovou Aplikaci.

---
+++ Webova Sluzba/Webova Sluzba/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webova_Sluzba.Models;
+
+namespace Webova_Sluzba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Cíl bakalářské práce.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Různé kontaktní údaje.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
